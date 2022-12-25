693c39b2136122dcbbd47dd15c3fc51f54bafff8
Oein <oein0219@gmail.com>
2022-12-25 14:48:21 +0900

    redis init
    Co-authored-by: awesomeDream <awesomeSwam@users.noreply.github.com>

---
+++ src/cache/prepareCache.ts
@@ -0,0 +1,12 @@
+import redis from "../database/redis";
+
+// DB 에 있는 데이터들 Redis 에 올리기
+export default async function prepareCahce() {
+  await redis.redisClient.flushall();
+  const data = await redis.prisma.school.findMany({});
+
+  data.forEach((e) => {
+    redis.pop.set(e.schoolCode, parseInt(e.pops.toString()));
+    redis.totalScore += Number(e.pops);
+  });
+}
