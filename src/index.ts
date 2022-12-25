bb6cd5538aa4fbe189b18cca982613a548932d27
Oein <oein0219@gmail.com>
2022-12-25 14:48:01 +0900

    MAIN
    Co-authored-by: awesomeDream <awesomeSwam@users.noreply.github.com>

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import "dotenv/config";
+
+// db
+import redis from "./database/redis";
+
+// middleware
+import rateLimiter from "./middleware/rateLimiter";
+
+// router
+import popRouter from "./router/pop";
+
+import prepareCahce from "./cache/prepareCache";
+import { checkPopQuery } from "./middleware/checkPopQuery";
+
+prepareCahce().then(() => {
+  console.log("[Cache]", "Updated Redis data");
+});
+
+redis.empty();
+
+let app = express();
+
+const PORT = parseInt(process.env.PORT || "8080");
+
+app.use("/pop", rateLimiter, checkPopQuery, popRouter);
+
+app.listen(PORT, () => {
+  console.log("[Express]", "Listening on port", PORT);
+});
