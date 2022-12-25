acc16b61582ed5b329a3a0d183737a1f7a404fae
Oein <oein0219@gmail.com>
2022-12-25 14:48:48 +0900

    pop router
    Co-authored-by: awesomeDream <awesomeSwam@users.noreply.github.com>

---
+++ src/router/pop.ts
@@ -0,0 +1,22 @@
+import express, { Router, Request, Response } from "express";
+import redis from "../database/redis";
+
+const popRouter: Router = express.Router();
+
+popRouter.post("/", async (req: Request, res: Response) => {
+  let pop = req.count || 0;
+  let schoolCode = req.schoolCode || "";
+  // 추가는 thread로
+  redis.pop.update(schoolCode, pop);
+
+  const ret = {
+    total: redis.totalScore,
+    rank: await redis.pop.getRank(schoolCode),
+    schoolPop: await redis.pop.getScore(schoolCode),
+    token: req.newToken,
+  };
+
+  return res.status(200).json(ret);
+});
+
+export default popRouter;
