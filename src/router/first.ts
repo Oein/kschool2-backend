1e4fa0a4bb1adb8a21ebdbcade187cd9ff42de1c
Oein <oein0219@gmail.com>
2022-12-26 20:18:42 +0900

    post -> get

--- src/router/first.ts
+++ src/router/first.ts
@@ -4,7 +4,7 @@ import validateSchool from "../validator/validateSchool";
 
 var firstRouter: Router = express.Router();
 
-firstRouter.post("/", async (req: Request, res: Response) => {
+firstRouter.get("/", async (req: Request, res: Response) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
   var schoolCode = req.query.schoolCode || "";
 
